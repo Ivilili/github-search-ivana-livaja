16fe1e93f54898285bf58d8cb5d95515a0890642
Ivana Livaja <ivana.livaja@hotmail.com>
2020-01-17 01:10:49 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Search from './components/Search';
 function App({ user, value, handleSubmit, handleChange }) {
 	return (
 		<div className="App">
-			<img src="./github.png" alt="github icon" className="hero" />
+			<img src="https://i.postimg.cc/rprgKKJJ/gh.png" alt="github icon" className="hero" />
 			<h1 className="title">GitHub Search</h1>
 			<Search value={value} onChange={handleChange} handleSubmit={handleSubmit} />
 		</div>
